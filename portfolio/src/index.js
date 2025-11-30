7a4e73612df85eb0bdc9a73faf9008bba09c860d
M-naveel <mnaveel051@gmail.com>
2025-12-01 02:30:34 +0500

    Update index.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-router/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
