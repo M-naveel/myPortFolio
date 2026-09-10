430b2f95534cdad0736cde0ef9b68635857b0bfc
M-naveel <mnaveel051@gmail.com>
2026-09-10 13:28:09 +0500

    Projects added
    Wordpress Projects have been added to my Portfolio

--- portfolio/src/Components/Project.jsx
+++ portfolio/src/Components/Project.jsx
@@ -6,6 +6,9 @@ import fp from '../Images/Porjects/foodpanda.png'
 import pg from '../Images/Porjects/password generator.png'
 import WC from '../Images/Porjects/WC.png'
 import cart from '../Images/Porjects/cart.png'
+import Restaurant from '../Images/Porjects/Reataurant.png'
+import Barber from '../Images/Porjects/barber.png'
+import Barber from '../Images/Porjects/Whitespace.png'
 
 
 
@@ -22,6 +25,12 @@ const projects = [
   {id:7 , pname:"Shopping Cart" , img:cart , domain:"https://naveel-cart.netlify.app/"},
 ]
 
+const wprojects = [
+  
+     {id:1 , pname:"Restaurant" , img:Restaurant , domain:"naveel-demo.infinityfree.me "},
+     {id:2 , pname:"Barber" , img:Barber , domain:"barbar.infinityfree.me "},
+     {id:3 , pname:"Whitespace" , img:Whitespace , domain:"https://whitespace.infinityfree.me/"},
+]
   return (
     <>
     <div className="container" >
@@ -31,6 +40,31 @@ const projects = [
          
          {
           projects.map(pcard=>(
+            <div key={pcard.id} className=" col-md-6"
+            >
+          
+            <img src={pcard.img} alt="" style={{ maxWidth:"100%"
+              , border:'2px solid white'
+            }} className='rounded-2' />
+            <h3 className='mt-2'><u>{pcard.pname}</u></h3>
+        
+            <a href={pcard.domain} className='btn ' target='blank'>
+            Click to Preview</a>
+           
+          
+          </div>
+          ))
+         }
+          
+        </div>
+      
+    </div>
+    <div className="container Wordpress">
+         <h2>Wordpress Projects</h2>
+         <div className="row g-3 px-2" id='Projects' >
+         
+         {
+          wprojects.map(pcard=>(
             <div key={pcard.id} className=" col-md-6"
             >
           
